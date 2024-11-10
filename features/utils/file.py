a4b60b4c12df184fb6d219240cacac906e2fdfba
Дамир <const0main@gmail.com>
2024-11-10 21:34:34 +0300

    Fixed a bug where media files (photos) were damaged, added small visual changes

--- features/utils/file.py
+++ features/utils/file.py
@@ -2,8 +2,8 @@ from features.encryption.methods import AESCipher
 from base64 import b64encode, b64decode
 from typing import Literal
 from shutil import rmtree
-from time import sleep
 from PIL import Image
+import magic
 import json
 import os
 
@@ -21,7 +21,30 @@ def is_image_file(file_path: str) -> bool:
         return False
 
 
+def is_media_file(filepath: str) -> bool:
+    video_extensions = {'.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv'}
+    audio_extensions = {'.mp3', '.wav', '.aac', '.flac', '.ogg', '.wma'}
+
+    filepath = filepath.replace("\\", "/")
+
+    if not os.path.isfile(filepath):
+        return False
+
+    _, ext = os.path.splitext(filepath)
+
+    if ext.lower() in video_extensions or ext.lower() in audio_extensions:
+        return True
+
+    return False
+
+
 def get_byte_code(directory: str, mode: Literal['r', 'rb'] = "r") -> str:
+    """
+    Get file bytecode
+    :param directory: path to file
+    :param mode: file read mode
+    :return: file bytecode
+    """
     try:
         with open(directory, mode) as file:
             return file.read()
@@ -37,19 +60,27 @@ def scan_directory(directory: str) -> list[str, dict]:
             for folder in dirs:
                 blocks.append({"folder": folder})
             for file in files:
-                if is_image_file(directory + file):
-                    blocks.append({"photo": file, "bytecode": b64encode(get_byte_code(directory + file, mode="rb")).decode()})
+                normalized_filepath = directory + file
+
+                if is_image_file(normalized_filepath):
+                    blocks.append({"photo": file, "bytecode": b64encode(get_byte_code(normalized_filepath, mode="rb")).decode()})
+                elif is_media_file(normalized_filepath):
+                    blocks.append({"video": file, "bytecode": b64encode(get_byte_code(normalized_filepath, mode="rb")).decode()})
                 else:
-                    blocks.append({"file": file, "bytecode": get_byte_code(directory + file)})
+                    blocks.append({"file": file, "bytecode": get_byte_code(normalized_filepath)})
         else:
             for folder in dirs:
                 relative_folder = os.path.relpath(os.path.join(root, folder), directory)
+
                 blocks.append({"folder": relative_folder})
             for file in files:
                 relative_file = os.path.relpath(os.path.join(root, file), directory)
+                normalized_filepath = directory + file
 
-                if is_image_file(directory + file):
-                    blocks.append({"photo": relative_file, "bytecode": b64encode(get_byte_code(directory + file, mode="rb")).decode()})
+                if is_image_file(normalized_filepath):
+                    blocks.append({"photo": relative_file, "bytecode": b64encode(get_byte_code(normalized_filepath, mode="rb")).decode()})
+                elif is_media_file(normalized_filepath):
+                    blocks.append({"video": file, "bytecode": b64encode(get_byte_code(normalized_filepath, mode="rb")).decode()})
                 else:
                     blocks.append({"file": relative_file, "bytecode": get_byte_code(directory + relative_file)})
 
@@ -97,8 +128,9 @@ def run_decryption(block_directory: str, master_password: str):
                 block_type = "folder"
             elif "file" in current_block_data.keys():
                 block_type = "file"
+            elif "video" in current_block_data.keys():
+                block_type = "video"
             else:
-                print("block type: photo")
                 block_type = "photo"
 
             if block_type == "folder":
@@ -110,6 +142,9 @@ def run_decryption(block_directory: str, master_password: str):
                     except TypeError:
                         # if 'bytecode' is null
                         pass
+            elif block_type == "video":
+                with open(insecure_path + current_block_data['video'], 'wb') as video_file:
+                    video_file.write(b64decode(current_block_data['bytecode']))
             else:
                 with open(insecure_path + current_block_data['photo'], 'wb') as image_file:
                     image_file.write(b64decode(current_block_data['bytecode']))
--- main.py
+++ main.py
@@ -1,44 +1,59 @@
+import os
+
 from features.encryption.methods import generate_static_code
 from features.utils.console import draw_string
 from features.screen.menu import render_gui
 from features.utils.file import *
+from time import sleep
 from init import *
 
-os.system("cls")
 
-while True:
-    draw_string(f"Version: {VERSION} {RELEASE}", message_type="information")
-    draw_string(f"Author: @{AUTHOR}", message_type="information")
+def main():
+    os.system("cls")
 
-    print("--------------------------------------")
+    while True:
+        try:
+            draw_string(f"Version: {VERSION} {RELEASE}", message_type="information")
+            draw_string(f"Author: @{AUTHOR}", message_type="information")
 
-    render_gui()
-    action_number = int(input("\n[ • ] Action number: "))
+            print("--------------------------------------")
 
-    match action_number:
-        case 1:
-            try:
-                directory = input("Directory: ")
-                block_name = input("Block name: ")
-                master_password = generate_static_code(input("Master password: "))
+            render_gui()
+            action_number = int(input("\n[ • ] Action number: "))
 
-                run_encryption(directory + "/", block_name, master_password)
+            match action_number:
+                case 1:
+                    try:
+                        directory = input("Directory: ")
+                        block_name = input("Block name: ")
+                        master_password = generate_static_code(input("Master password: "))
 
-                draw_string("The block was successfully encrypted", message_type="success")
-            except Exception as e:
-                draw_string(f"An error occurred while encrypting the block: {e}", message_type="error")
-        case 2:
-            try:
-                directory = input("Block directory: ")
-                master_password = generate_static_code(input("Master password: "))
+                        run_encryption(directory + "/", block_name, master_password)
 
-                run_decryption(directory + "/", master_password)
+                        draw_string("The block was successfully encrypted", message_type="success")
+                    except Exception as e:
+                        draw_string(f"An error occurred while encrypting the block: {e}", message_type="error")
+                case 2:
+                    try:
+                        directory = input("Block directory: ")
+                        master_password = generate_static_code(input("Master password: "))
 
-                draw_string("The block was successfully decrypted", message_type="success")
-            except Exception as e:
-                draw_string(f"An error occurred while decrypting the block: {e}", message_type="error")
-        case 3:
-            break
+                        run_decryption(directory + "/", master_password)
 
-    sleep(3)
-    os.system('cls')
+                        draw_string("The block was successfully decrypted", message_type="success")
+                    except Exception as e:
+                        draw_string(f"An error occurred while decrypting the block: {e}", message_type="error")
+                case 3:
+                    break
+
+            sleep(3)
+            os.system('cls')
+        except ValueError:
+            draw_string("Такого варианта не существует", message_type="error")
+            sleep(2)
+            os.system("cls")
+
+
+if __name__ == '__main__':
+    main()
+
